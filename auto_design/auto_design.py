fix torque calculation

auto_design/auto_design.py
@@ -15,10 +15,11 @@ from modules.interference_removal import InterferenceRemoval, RobotOptResult
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='Mesh Loader')
-    parser.add_argument('--model_name', type=str, default='lynel', help='The model name')
+    parser.add_argument('--model_name', type=str, default='gold_lynel', help='The model name')
     parser.add_argument('--expected_x', type=float, default=50, help='The expected width of the model')
-    parser.add_argument('--voxel_size', type=float, default=0.5, help='The size of the voxel')
-    parser.add_argument('--voxel_density', type=float, default=2e-5, help='The density of the voxel')
+    parser.add_argument('--voxel_size', type=float, default=1.0, help='The size of the voxel')
+    ## TODO: change the voxel density
+    parser.add_argument('--voxel_density', type=float, default=2e-4, help='The density of the voxel')
     args = parser.parse_args()
     mesh_path = os.path.normpath('./auto_design/model/given_models/' + args.model_name + '.stl')
     joint_path = os.path.normpath('./auto_design/model/given_models/' + args.model_name + '_joints.pkl')
@@ -39,7 +40,7 @@ if __name__=="__main__":
                 #  [4.5, 2.5, 8 ], # DM4310
                  [8.1, 5.3, 20 ]]  # DM8009
     motor_opt = Motor_Opt(args, mesh_decomp, bounds, motor_lib)
-    motor_results = motor_opt.run_opt(generation_num=50)
+    motor_results = motor_opt.run_opt(generation_num=10)
     motor_opt.render()
     joint_connect_opt = Joint_Connect_Opt(args, mesh_decomp, motor_opt.motor_results)
     joint_connect_opt.run_opt()
@@ -49,7 +50,7 @@ if __name__=="__main__":
                                                motor_param_result=motor_results, 
                                                link_tree=mesh_decomp.link_tree, 
                                                father_link_dict=mesh_decomp.father_link_dict)
-    joint_limits = np.vstack([np.array([-0.3, 0.3]) for _ in range(2*len(motor_results))])
+    joint_limits = np.vstack([np.array([-0.5, 0.5]) for _ in range(2*len(motor_results))])
     interference_removal.set_joint_limit(joint_limits)
     interference_removal.remove_interference()
     interference_removal.mesh_group.render()

auto_design/modules/interference_removal.py
@@ -362,20 +362,36 @@ class InterferenceRemoval:
                 self.mesh_group.voxel_data[new_indexs[:, 0], new_indexs[:, 1], new_indexs[:, 2]] = np.where(np.logical_or(np.isin(values, transformed_link_values), non_removal),
                                                                                                             self.mesh_group.voxel_data[new_indexs[:, 0], new_indexs[:, 1], new_indexs[:, 2]], 
                                                                                                             0)
-                def condition_remove(pts):
-                    return is_points_in_cylinder(pts, self.motor_param_result[cur_idx-1][:3], self.motor_param_result[cur_idx-1][3:6], self.motor_param_result[cur_idx-1][6], 0, 0)
-                self.mesh_group.move_voxels(initial_group_names=get_removed_list(list(self.mesh_group.link_value_dict.keys()), "Unoccupied"),
-                                            target_group_name="Unoccupied",
-                                            condition_func=condition_remove)
 
             # Leave place for the second motor
             if len(current_link.axis) == 3:
-                def condition_remove(pts):
-                    return is_points_in_cylinder(pts, self.motor_param_result[cur_idx][:3], self.motor_param_result[cur_idx][3:6], self.motor_param_result[cur_idx][6], 0, 0)
-                self.mesh_group.move_voxels(initial_group_names=get_removed_list(list(self.mesh_group.link_value_dict.keys()), "Unoccupied"),
-                                            target_group_name="Unoccupied",
-                                            condition_func=condition_remove)
+
+                motor_position = (self.motor_param_result[cur_idx][:3] + self.motor_param_result[cur_idx][3:6]) / 2
+                motor_direct = np.array(current_link.axis[1])
+                motor_radius = self.motor_param_result[cur_idx][6]
+
+                # Sample joint angles and implement the interference removal
+                for joint_angle in np.linspace(self.joint_limits[cur_idx, 0], self.joint_limits[cur_idx, 1], 10):
+                    transformed_voxel_data = self.mesh_group.voxel_data.copy()
+
+                    child_nodes = current_node.get_all_children()[0]
+                    transformed_links = [child.val for child in child_nodes]
+                    transformed_links.append(current_link)
+                    transformed_link_names = [link.name for link in transformed_links]
+                    transformed_link_values = [self.mesh_group.link_value_dict[link_name] for link_name in transformed_link_names]
+                    transformed_indexs = np.vstack([self.mesh_group.get_voxels(link_name, get_index=True) for link_name in transformed_link_names])
+
+                    # The transformation is defined as rotation around the axis of the motor, rotating angle is the joint angle
+                    H_matrix = self.rotate_around_axis(motor_direct, joint_angle, self.mesh_group.position_to_index(motor_position.reshape(-1, 3)))
+                    new_indexs = expand_points(apply_transform(transformed_indexs, H_matrix))
+                    new_indexs = np.clip(new_indexs, 0, self.mesh_group.voxel_data.shape[0]-1)
+                    values = self.mesh_group.voxel_data[new_indexs[:, 0], new_indexs[:, 1], new_indexs[:, 2]]
+                    non_removal = self.mesh_group.voxel_no_removal[new_indexs[:, 0], new_indexs[:, 1], new_indexs[:, 2]]
+                    self.mesh_group.voxel_data[new_indexs[:, 0], new_indexs[:, 1], new_indexs[:, 2]] = np.where(np.logical_or(np.isin(values, transformed_link_values), non_removal),
+                                                                                                                self.mesh_group.voxel_data[new_indexs[:, 0], new_indexs[:, 1], new_indexs[:, 2]], 
+                                                                                                                0)
                 cur_idx += 1
+            
 
     def generate_urdf(self):
         """

auto_design/modules/mesh_decomp.py
@@ -470,6 +470,30 @@ class Mesh_Decomp:
                         "limit": {"lower": "-1.57", "upper": "1.57", "effort": "20", "velocity": "1.5"}
                     }
                     write_joint(urdf_file, **cur_joint)
+        
+        # Writre foot link
+        def contact_link_func(link):
+            for joint_name in link.joints.keys():
+                if 'foot' in joint_name:
+                    return True
+            return False
+        contact_nodes = self.link_tree.find_children(contact_link_func, None)
+        for contact_node in contact_nodes:
+            for joint_name, joint_pos in contact_node.val.joints.items():
+                if 'foot' in joint_name:
+                    write_link(urdf_file=urdf_file, link_name=joint_name)
+                    rel_pos = np.array(np.array(joint_pos) - contact_node.val.axis[0]) / 100.0
+                    foot_joint = {
+                        "joint_name": joint_name + '_joint',
+                        "joint_type": "revolute",
+                        "parent_link": contact_node.val.name,
+                        "child_link": joint_name,
+                        "origin": {"xyz": ' '.join(map(str, rel_pos)), "rpy": "0 0 0"},
+                        "axis": {"xyz": "1 0 0"},
+                        "limit": {"lower": "-1.57", "upper": "1.57", "effort": "0", "velocity": "0"}
+                    }
+                    write_joint(urdf_file=urdf_file, **foot_joint)
+                    break
 
         urdf_file.write('<gazebo>\n')
         urdf_file.write('   <plugin filename="libgazebo_ros_p3d.so" name="p3d_base_controller">\n')
@@ -505,57 +529,30 @@ class Mesh_Decomp:
         # Build a data frame associated with the model
         data = model.createData()
         max_torque = np.zeros((model.nv))
-
-        # Get the support force and related joint and link
-        def contact_link_func(link):
-            for joint_name in link.joints.keys():
-                if 'foot' in joint_name:
-                    return True
-            return False
-        
-        contact_nodes = self.link_tree.find_children(contact_link_func, None)
-        contact_transformations = []
-        contact_links = []
-        for contact_node in contact_nodes:
-            contact_links.append(contact_node.val.name+ '_joint')
-            for joint_name, joint_pos in contact_node.val.joints.items():
-                if 'foot' in joint_name:
-                    contact_transformations.append(np.array(np.array(joint_pos) - contact_node.val.axis[0]) / 100.0)
-                    break
-
-        # Contact force that will be applied to foot links
         force = np.array([0, 0, 9.81 * self.ideal_mass / 2])
 
         # Sample a random joint configuration, joint velocities and accelerations
-        for i in range(10):
+        for i in range(100):
             q = pin.randomConfiguration(model)
-            # q = 1.57 * np.ones((model.nq, 1))  # in rad
-            # q = np.array([0.0, 0.0, 0.0, -1.57, 1.57, 1.57, -1.57, 1.57, -1.57, -1.57, 1.57]).reshape(-1, 1)
             v = np.zeros((model.nv, 1))  # in rad/s 
             a = np.zeros((model.nv, 1))  # in rad/s² 
             
             # Add external forces to the link which contains the joint whose name contains 'foot'    
             fs_ext = [pin.Force(np.array([0,0,0,0,0,0])) for _ in range(len(model.joints))]
-
             for i, joint_name in enumerate(model.names):
-                if joint_name in contact_links:
-                    contact_id = contact_links.index(joint_name)
-                    transformed_torque = np.cross(contact_transformations[contact_id], force)
-                    fs_ext[i] = pin.Force(np.hstack((force, transformed_torque)))
-                    # print(f'External force: {fs_ext[i].vector}')
+                if 'foot' in joint_name:
+                    fs_ext[i] = pin.Force(np.hstack((force, np.zeros(3))))
             tau = pin.rnea(model, data, q, v, a, fs_ext)
-
-            # print(f'Configuration: {np.round(q.flatten(), 2)}')
-            # print(f'Joint torques: {np.round(tau.flatten(), 2)}')
-            
             max_torque = np.maximum(max_torque, np.abs(tau))
 
         # Get joint names
         joint_names = []
-        for joint_name in model.names:
-            if joint_name != 'universe':
+        result_torque = []
+        for i, joint_name in enumerate(model.names):
+            if (joint_name != 'universe') and ('foot' not in joint_name):
                 joint_names.append(joint_name)
-        return joint_names, np.round(max_torque, 2)
+                result_torque.append(np.round(max_torque[i-1], 2))
+        return joint_names, result_torque
 
     def render(self):
         """