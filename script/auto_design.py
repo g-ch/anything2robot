946e4c01531f63b08b3c831b54e04cb25d6657fd
g-ch <947089399@qq.com>
2024-10-26 01:11:45 +0200

    corrected the motor library

--- script/auto_design.py
+++ script/auto_design.py
@@ -36,16 +36,16 @@ class MotorParameterLib:
         self.motor_lib = [        
                           [2.25, 2.15, 0.9], # GIM3505. SITAIWEI
                           [3.42, 2.65, 2.5], # MG4005V2. K-Tech
-                          [5.6, 4.2, 12],   # DM6006. DAMIAO Tech 
-                          [3.75, 4.8, 20], # DM8006. DAMIAO Tech
+                          [3.65, 3.8, 12],   # DM6006. DAMIAO Tech 
+                          [4.0, 4.8, 20], # DM8006. DAMIAO Tech
                           [6.2, 5.56, 120], # DM10010. DAMIAO Tech
                          ]  
         
         # This is the connector length between two motors in a 2 DOF joint. L shape. Unit: cm
         self.connector_lib = []
         for i in range(len(self.motor_lib)):
-            # Connect is (motor_height + motor_radius) * 0.5 + 1
-            connector_length = (self.motor_lib[i][0] + self.motor_lib[i][1]) * 0.5 + 1
+            # connector_length is radius + height/2 + 0.5. 0.5 is a margin.
+            connector_length = self.motor_lib[i][1] + self.motor_lib[i][0]/2 + 0.5
             self.connector_lib.append([connector_length, connector_length])
 
         # Add self.tenon_height to the motor height to consider the tenon height
